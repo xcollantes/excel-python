e52f0f5b6318006914841fb8607f675fda08d5b9
xcollantes <xcollantes@users.noreply.github.com>
2018-09-02 16:31:11 -0700

    Fucking around

--- Main.py
+++ Main.py
@@ -3,31 +3,22 @@
 # Examples tried out from Al Sweigart's book, "Automating the Boring Stuff with Python"
 # Excel Automation: Chapter 12
 
-import openpyxl
+import openpyxl, os
 
 
 def t(obj):
 	print (type(obj))
 	
 	
-if __name__ == '__main__':
-	xlsx = 'xl/example.xlsx'
-	wb = openpyxl.load_workbook(xlsx)
-	print(wb.sheetnames)
-	fsheet = wb['Fruits']
-	
-	print(type(fsheet['A1'].value))
-	print(type(fsheet['ZZ1'].value))
-	fsheet['ZZ1'].value = 'X'
-	print(fsheet['ZZ1'].value)
-	
-	m = fsheet['B1']
-	print("Cell: " + str(m.coordinate))
-	print("Row: " + str(m.row) + "|  Column: " + str(m.column))
+def newSheet():
+	wb = openpyxl.Workbook()
 	
 	
-	
-	
-	
+	ws = wb.create_sheet("Anotha One")
+	wb.create_sheet("Anotha Anotha One")
 
 	
+	
+	wb.save('./xl/myWS.xlsx')
+	
+newSheet()
