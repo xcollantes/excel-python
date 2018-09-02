ef38a362594f90928d6a75b405468f92fb099946
xcollantes <xcollantes@users.noreply.github.com>
2018-09-02 16:03:23 -0700

    Test functions

--- Main.py
+++ Main.py
@@ -5,3 +5,29 @@
 
 import openpyxl
 
+
+def t(obj):
+	print (type(obj))
+	
+	
+if __name__ == '__main__':
+	xlsx = 'xl/example.xlsx'
+	wb = openpyxl.load_workbook(xlsx)
+	print(wb.sheetnames)
+	fsheet = wb['Fruits']
+	
+	print(type(fsheet['A1'].value))
+	print(type(fsheet['ZZ1'].value))
+	fsheet['ZZ1'].value = 'X'
+	print(fsheet['ZZ1'].value)
+	
+	m = fsheet['B1']
+	print("Cell: " + str(m.coordinate))
+	print("Row: " + str(m.row) + "|  Column: " + str(m.column))
+	
+	
+	
+	
+	
+
+	
